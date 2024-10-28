e21ebad196b333a735030dc3de0a8e3dc12a447a
Mell Stephania Arguello Falcon <mell.arguello@udla.edu.ec>
2024-10-28 10:02:24 -0500

    6 validaciones diferentes (Data Annotations)

--- Models/MA_Burger.cs
+++ Models/MA_Burger.cs
@@ -5,9 +5,17 @@ namespace MellArguello_EXAMENP1.Models
     public class MA_Burger
     {
         public int MA_Id { get; set; }
+        [Key]
         public string? MA_Nombre { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres.")]
         public bool MA_ConsumidorFinal { get; set; }
+        [Display(Name = "Consumidor Final")]
         public decimal MA_Precio { get; set; }
+        [Range(0.01, 9999.99, ErrorMessage = "El precio debe estar entre 0.01 y 9999.99.")]
+        [DataType(DataType.Currency)]
         public DateTime MA_Fecha { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
     }
 }
